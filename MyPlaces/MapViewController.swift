ba541be28be545bfdeb3285039c2a226b5dc35aa
Faww <weedsm@mail.ru>
2022-04-11 14:52:22 +0300

    map kit, core location

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,165 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by MacBook on 10.04.2022.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class MapViewController: UIViewController {
+    
+    var place = Place()
+    let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
+    let regionMeters = 10_000.00
+    var incomeSegueIdentifier = ""
+
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var mapPinImage: UIImageView!
+    @IBOutlet var doneButton: UIButton!
+    @IBOutlet var addressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        setupMapView()
+        checkLocationServises()
+
+    }
+    
+    @IBAction func centerViewInUserLocation() {
+        showUserLocation()
+    }
+    @IBAction func doneButtonPressed() {
+    }
+    
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+    private func setupMapView() {
+        if incomeSegueIdentifier == "showPlace" {
+            setupPlacemark()
+            mapPinImage.isHidden = true
+            addressLabel.isHidden = true
+            doneButton.isHidden = true
+        }
+    }
+    
+    private func setupPlacemark() {
+        guard let location = place.location else { return }
+        
+       let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+           
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
+    }
+    
+    private func checkLocationServises() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+               self.showAlert(title: "Your location are Disabled", message: "To enable: Settings -> Privacy -> Location services turn on")
+            }
+        }
+    }
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    private func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            if incomeSegueIdentifier == "getAddress" { showUserLocation() }
+            break
+        case .denied:
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Your location is not avaliable", message: "To give permission go to: Settings -> My Places -> Location")
+            }
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print("New case is avaliable")
+        }
+    }
+    
+    private func showUserLocation() {
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: regionMeters, longitudinalMeters: regionMeters)
+            
+            mapView.setRegion(region, animated: true)
+        }
+          
+    }
+    
+    private func showAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+       
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        if let imageData = place.imageData {
+            
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        return annotationView
+    }
+}
+extension MapViewController: CLLocationManagerDelegate{
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+}
